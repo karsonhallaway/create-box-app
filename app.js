031542c2916daa5562989b70c159272258ae6354
Karson Hallaway <karsonhallaway@gmail.com>
2025-11-24 08:09:18 -0700

    added createBox function and removeAllBoxes function

---
+++ app.js
@@ -0,0 +1,19 @@
+const createBoxButton = document.getElementById("createBox");
+
+const removeBoxesButton = document.getElementById("removeBoxes");
+
+const boxContainer = document.getElementById("boxContainer");
+
+function createBox() {
+
+}
+
+function removeAllBoxes() {
+
+}
+
+// listen to click events on the two buttons
+createBoxButton.addEventListener("click", createBox);
+
+removeBoxesButton.addEventListener("click", removeAllBoxes);
+
